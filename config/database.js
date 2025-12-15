880176facefe6774204ac0d1d9277fa8ad42f50b
dev-bakery <rimming2255@naver.com>
2025-12-15 10:44:14 +0900

    database.js 수정

--- config/database.js
+++ config/database.js
@@ -1,29 +1,11 @@
 const path = require("path");
 
-module.exports = ({ env }) => {
-  const isProduction = env("NODE_ENV") === "production";
-
-  // ⭐ 로컬 개발 환경 = SQLite
-  if (!isProduction) {
-    return {
-      connection: {
-        client: "sqlite",
-        connection: {
-          filename: path.join(__dirname, "..", ".tmp/data.db"),
-        },
-        useNullAsDefault: true,
-      },
-    };
-  }
-
-  // ⭐ Render(배포) 환경 = PostgreSQL
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: "sqlite",
     connection: {
-      client: "postgres",
-      connection: {
-        connectionString: env("DATABASE_URL"),
-        ssl: false,
-      },
+      filename: path.join(__dirname, "..", ".tmp/data.db"),
     },
-  };
-};
+    useNullAsDefault: true,
+  },
+});
